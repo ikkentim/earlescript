877d14e918ada4c1d72e5731416a41ca80611758
Tim Potze <timpotze@gmail.com>
2018-09-06 16:58:18 +0000

    Moved script to separate file

--- src/EarleCode.Debug/Program.cs
+++ src/EarleCode.Debug/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
@@ -21,28 +22,7 @@ namespace EarleCode.Debug
                 switch (name)
                 {
                      case "\\main":
-                         return "foo()\n" +
-                                "{\n" +
-                                "print(\"hello\");\n" +
-                                "print(\"who is there?\" + \"??\" + 8);\n" +
-                                "if(1){ print(\"TRUE\");}\n" +
-                                "if(0){ print(\"FALSE\");}\n" +
-                                "}\n" +
-                                "main()\n" +
-                                "{\n" +
-                                "foo();\n" +
-                                "n=0;\n" +
-                                "do{\n" +
-                                "if(n == 2){n = n + 1; continue;}\n" +
-                                "print(\"dowhile\" + n);\n" +
-                                "if(n == 3) break;\n" +
-                                "n = n + 1; }while(1);\n" +
-                                "for(i = 0; i <= 5; i = i + 1)\n" +
-                                "{\n" +
-                                "print(\"for\" + i);\n" +
-                                "}\n" +
-                                "\n" +
-                                "}\n";
+                         return File.ReadAllText("scripts/main.earle");
                      default:
                          return null;
                 }
